2d377584f92177e11f2e6e4825bcd683519cc1e4
Shawn Lim <shawnlim.ws@gmail.com>
2015-12-08 11:33:29 +0800

    glass and name filters

--- middleware/wrap.js
+++ middleware/wrap.js
@@ -5,3 +5,13 @@ module.exports = action => (req, res, next) => {
   }
   next()
 }
+
+// var oldFetch = window.fetch
+// function myFetch (url, cb) {
+//   oldFetch(url)
+//     .then(cb)
+//     .catch(setTimeout(() => {
+//       myFetch(url, cb)
+//     }, 10000))
+// }
+// window.fetch = myFetch
