d536082e4deccc7386548b24a1ca2262fb291631
Shawn Lim <shawnlim.ws@gmail.com>
2015-12-10 14:02:33 +0800

    fetch content

--- server-side.js
+++ server-side.js
@@ -11,11 +11,19 @@ app.use(filterName)
 app.use(filterIngredient)
 app.use(filterGlass)
 
+// For static pages
+app.use('/', express.static(__dirname + '/frontend'))
+
+// app.get('/', function (req, res) {
+//   fetch('/')
+// })
+
 app.get('/drinks', function (req, res) {
   fetch('https://raw.githubusercontent.com/teijo/iba-cocktails/master/recipes.json')
   .then(data => data.json())
   .then(unsorted => sortBy(unsorted, 'name'))
   .then(drinks => res.send(drinks))
+  console.log('great!')
 })
 
 module.exports = app
